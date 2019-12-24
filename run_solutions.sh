51bbd072cd2cd429e4ee1b6f5ad816587708ec1a
PPanda <ppanda@Anonymous.local>
2019-12-23 19:45:42 -0800

    Added empty .py files and inputs for all days

--- run_solutions.sh
+++ run_solutions.sh
@@ -56,3 +56,147 @@ printf "\n"
 printf "DAY 7 PART 2: "
 time python3 day07-2.py inputs/day07.txt
 printf "\n"
+
+printf "DAY 8 PART 1: "
+time python3 day08-1.py inputs/day08.txt 25 6
+printf "\n"
+
+printf "DAY 8 PART 2:\n\n"
+time python3 day08-2.py inputs/day08.txt 25 6
+printf "\n"
+
+printf "DAY 9 PART 1: "
+time python3 day09-1.py inputs/day09.txt
+printf "\n"
+
+printf "DAY 9 PART 2: "
+time python3 day09-2.py inputs/day09.txt
+printf "\n"
+
+printf "DAY 10 PART 1: "
+time python3 day10-1.py inputs/day10.txt
+printf "\n"
+
+printf "DAY 10 PART 2: "
+time python3 day10-2.py inputs/day10.txt
+printf "\n"
+
+printf "DAY 11 PART 1: "
+time python3 day11-1.py inputs/day11.txt
+printf "\n"
+
+printf "DAY 11 PART 2: "
+time python3 day11-2.py inputs/day11.txt
+printf "\n"
+
+printf "DAY 12 PART 1: "
+time python3 day12-1.py inputs/day12.txt
+printf "\n"
+
+printf "DAY 12 PART 2: "
+time python3 day12-2.py inputs/day12.txt
+printf "\n"
+
+printf "DAY 13 PART 1: "
+time python3 day13-1.py inputs/day13.txt
+printf "\n"
+
+printf "DAY 13 PART 2: "
+time python3 day13-2.py inputs/day13.txt
+printf "\n"
+
+printf "DAY 14 PART 1: "
+time python3 day14-1.py inputs/day14.txt
+printf "\n"
+
+printf "DAY 14 PART 2: "
+time python3 day14-2.py inputs/day14.txt
+printf "\n"
+
+printf "DAY 15 PART 1: "
+time python3 day15-1.py inputs/day15.txt
+printf "\n"
+
+printf "DAY 15 PART 2: "
+time python3 day15-2.py inputs/day15.txt
+printf "\n"
+
+printf "DAY 16 PART 1: "
+time python3 day16-1.py inputs/day16.txt
+printf "\n"
+
+printf "DAY 16 PART 2: "
+time python3 day16-2.py inputs/day16.txt
+printf "\n"
+
+printf "DAY 17 PART 1: "
+time python3 day17-1.py inputs/day17.txt
+printf "\n"
+
+printf "DAY 17 PART 2: "
+time python3 day17-2.py inputs/day17.txt
+printf "\n"
+
+printf "DAY 18 PART 1: "
+time python3 day18-1.py inputs/day18.txt
+printf "\n"
+
+printf "DAY 18 PART 2: "
+time python3 day18-2.py inputs/day18.txt
+printf "\n"
+
+printf "DAY 19 PART 1: "
+time python3 day19-1.py inputs/day19.txt
+printf "\n"
+
+printf "DAY 19 PART 2: "
+time python3 day19-2.py inputs/day19.txt
+printf "\n"
+
+printf "DAY 20 PART 1: "
+time python3 day20-1.py inputs/day20.txt
+printf "\n"
+
+printf "DAY 20 PART 2: "
+time python3 day20-2.py inputs/day20.txt
+printf "\n"
+
+printf "DAY 21 PART 1: "
+time python3 day21-1.py inputs/day21.txt
+printf "\n"
+
+printf "DAY 21 PART 2: "
+time python3 day21-2.py inputs/day21.txt
+printf "\n"
+
+printf "DAY 22 PART 1: "
+time python3 day22-1.py inputs/day22.txt
+printf "\n"
+
+printf "DAY 22 PART 2: "
+time python3 day22-2.py inputs/day22.txt
+printf "\n"
+
+printf "DAY 23 PART 1: "
+time python3 day23-1.py inputs/day23.txt
+printf "\n"
+
+printf "DAY 23 PART 2: "
+time python3 day23-2.py inputs/day23.txt
+printf "\n"
+
+printf "DAY 24 PART 1: "
+time python3 day24-1.py inputs/day24.txt
+printf "\n"
+
+printf "DAY 24 PART 2: "
+time python3 day24-2.py inputs/day24.txt
+printf "\n"
+
+printf "DAY 25 PART 1: "
+time python3 day25-1.py inputs/day25.txt
+printf "\n"
+
+printf "DAY 25 PART 2: "
+time python3 day25-2.py inputs/day25.txt
+printf "\n"
