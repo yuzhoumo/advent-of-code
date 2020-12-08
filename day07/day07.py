a18a42e7eac023b9fcef581f87be36cd7f7f94a6
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2020-12-08 13:07:41 -0800

    Fix bug in day07 where part1 solution was subtracted by 1, optimize day07 recursion with lru_cache

--- day07/day07.py
+++ day07/day07.py
@@ -1,5 +1,9 @@
 import sys
 import re
+from functools import lru_cache
+
+
+RULES = {}  # Global variable for storing bag rules, set in main
 
 
 def parse_input(lines):
@@ -33,13 +37,14 @@ def parse_input(lines):
 
 
 # Part 1
-def search_bags(rules, root, color):
+@lru_cache(maxsize=128)
+def search_bags(root, color):
     """
     Returns True if a bag of color `root` will eventually contain a child bag
     of color `color`, False otherwise.
     """
 
-    children = rules[root]
+    children = RULES[root]
 
     if children is None:
         return False
@@ -48,31 +53,37 @@ def search_bags(rules, root, color):
         return True
 
     for c in children:
-        if search_bags(rules, c, color):
+        if search_bags(c, color):
             return True
 
     return False
 
 
 # Part 2
-def count_bags(rules, root):
+@lru_cache(maxsize=128)
+def count_bags(root):
     """
-    Counts the number of total sub-bags given `rules` and a bag of color `root`.
+    Counts the number of total sub-bags given global `RULES` and a bag of
+    color `root`.
     """
 
-    children = rules[root]
+    children = RULES[root]
 
     if children is None:
         return 1
 
+    # Number of bags in the current level
     count = sum(children.values())
+
     for key in children.keys():
-        count += children[key] * count_bags(rules, key)
+        count += children[key] * count_bags(key)
 
     return count
 
 
 def main():
+
+    global RULES
 
     assert len(sys.argv) > 1, 'Missing argument: path to input file'
     assert len(sys.argv) < 3, 'Too many arguments'
@@ -81,12 +92,12 @@ def main():
     with open(input_file, 'r') as f:
         text = f.read().strip()
         lines = text.splitlines()
-        rules = parse_input(lines)
-        colors = rules.keys()
+        RULES = parse_input(lines)
+        colors = RULES.keys()
 
     # Solve for parts 1 and 2
-    part1 = sum(1 for c in colors if search_bags(rules, c, 'shiny gold')) - 1
-    part2 = count_bags(rules, 'shiny gold')
+    part1 = sum(1 for c in colors if search_bags(c, 'shiny gold'))
+    part2 = count_bags('shiny gold')
 
     print('\nInput:\n{0}'.format(text))
     print('\nPart 1:', part1)
