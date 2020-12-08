e3f249aeea1a83753130002a76f7c5ce8279c3af
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2020-12-08 12:39:03 -0800

    Add comments and clean up day01 to day06

--- day03/day03.py
+++ day03/day03.py
@@ -2,6 +2,14 @@ import sys
 
 # Part 1
 def count_trees(grid, rise, run):
+    """
+    Given a slope and starting from the top left corner, count
+    number of tree collisions on the map `grid`, which is a list
+    of strings where '#' represents a tree. The grid pattern repeats
+    horizontally and the function ends upon hitting the bottom row
+    of the grid.
+    """
+
     r, c, cnt = 0, 0, 0
     rows, cols = len(grid), len(grid[0])
 
@@ -14,6 +22,12 @@ def count_trees(grid, rise, run):
 
 # Part 2
 def count_product(grid, slopes):
+    """
+    Given a map `grid` and a list of slopes, perform `count_trees`
+    on each slope to get the number of tree collisions, and return
+    the product of all of the results.
+    """
+
     product = 1
 
     for slope in slopes:
@@ -29,15 +43,15 @@ def main():
 
     with open(input_file, 'r') as f:
         text = f.read().strip()
-        rows = text.splitlines()
+        grid = text.splitlines()
+
+    # Solve part 1 and 2
+    part1 = count_trees(grid, 1, 3)
+    part2 = count_product(grid, [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)])
 
     print('\nInput:\n{0}'.format(text))
-    print('\nPart 1:', count_trees(rows, 1, 3))
-
-    # Part 2 slopes
-    slopes = [(1, 1), (1, 3), (1, 5), (1, 7), (2, 1)]
-
-    print('\nPart 2:', count_product(rows, slopes))
+    print('\nPart 1:', part1)
+    print('\nPart 2:', part2)
 
 
 if __name__ == '__main__':
--- day05/day05.py
+++ day05/day05.py
@@ -1,9 +1,19 @@
 import sys
 
 # Part 1
-def get_id(chars):
+def get_id(code):
+    """
+    Given `code`, a string representing a boarding pass seat code,
+    convert into a seat id, where `code` is a base 2 number such
+    that 'B' -> 1, 'F' -> 0, 'R' -> 1, 'L' -> 0.
+
+    Example: 'BFFFBBFRRR' -> 0b1000110111
+        >>> get_id('BFFFBBFRRR')
+        >>> 567
+    """
+
     n, tot = 1, 0
-    for c in chars[::-1]:
+    for c in code[::-1]:
         if c == 'B' or c == 'R':
             tot += n
         n *= 2
@@ -12,12 +22,21 @@ def get_id(chars):
 
 
 # Part 2
-def get_missing(ids):
+def get_missing(seat_ids):
+    """
+    Given an unsorted list of consecutive seat ids, get the missing
+    seat, where it is the only non-consecutive gap in the list (assume
+    this number exists exactly once), return None otherswise.
 
-    ids = sorted(ids)
-    curr = ids[0]
+    Example:
+        >>> get_missing([1, 4, 3, 5])
+        >>> 2
+    """
 
-    for n in ids:
+    seat_ids = sorted(seat_ids)
+    curr = seat_ids[0]
+
+    for n in seat_ids:
         if curr != n:
             return curr
         curr += 1
@@ -33,11 +52,15 @@ def main():
         seats = text.splitlines()
 
     # Get list of seat ids
-    ids = [get_id(s) for s in seats]
+    seat_ids = [get_id(s) for s in seats]
+
+    # Solve parts 1 and 2
+    part1 = max(seat_ids)
+    part2 = get_missing(seat_ids)
 
     print('\nInput:\n{0}'.format(text))
-    print('\nPart 1:', max(ids))
-    print('\nPart 2:', get_missing(ids))
+    print('\nPart 1:', part1)
+    print('\nPart 2:', part2)
 
 
 if __name__ == '__main__':
