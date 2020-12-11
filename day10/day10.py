f8450aaa8f67b164a281587702132cd9320ebf33
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2020-12-11 13:42:22 -0800

    Add comments to day10

--- day10/day10.py
+++ day10/day10.py
@@ -4,6 +4,11 @@ from collections import defaultdict
 
 # Part 1
 def ones_threes_product(sorted_nums):
+    """
+    Given a sorted list of integers, count the number of elements in the
+    list such that the difference between the current and previous elements
+    are exactly 1 or 3. Return the product of these two counts.
+    """
 
     # prev = 0 (wall jolts), threes = 1 (device jolts)
     prev, ones, threes = 0, 0, 1
@@ -21,6 +26,11 @@ def ones_threes_product(sorted_nums):
 
 # Part 2
 def num_arrangements(sorted_nums):
+    """
+    Count the number of subsets of a sorted list of integers such that
+    the numbers span from 0 to max(sorted_nums) + 3 with differences
+    of <= 3 between each number.
+    """
 
     counts = defaultdict(int)
     counts[0] = 1
@@ -36,17 +46,15 @@ def main():
     assert len(sys.argv) < 3, 'Too many arguments'
     input_file = sys.argv[1]
 
+    # Read input as a sorted list of integers
     with open(input_file, 'r') as f:
         text = f.read().strip()
         nums = [int(n) for n in text.splitlines()]
+        nums.sort()
 
     # Solve for parts 1 and 2
-    nums.sort()
-    part1 = ones_threes_product(nums)
-    part2 = num_arrangements(nums)
-
-    print('\nPart 1:', part1)
-    print('Part 2:', part2)
+    print('\nPart 1:', ones_threes_product(nums))
+    print('Part 2:', num_arrangements(nums))
 
 
 if __name__ == '__main__':
