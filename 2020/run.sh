262ee6b09476a0b7263d90792db59a56fa7021aa
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2021-12-01 10:40:22 -0800

    Remove empty files from 2020

--- 2020/run.sh
+++ 2020/run.sh
@@ -3,7 +3,7 @@
 echo
 echo Advent of Code 2020
 
-for n in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
+for n in 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15
 do
     echo
     echo Day $n:
