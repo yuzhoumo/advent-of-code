45e4701094952b5cb7bff2df7a824be5605513b9
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2022-12-09 22:53:53 -0800

    Add 2022 scaffolding for rust

---
+++ 2022/src/bin/read.rs
@@ -0,0 +1,42 @@
+use advent_of_code::aoc_cli;
+use std::process;
+
+struct Args {
+    day: u8,
+    year: Option<u16>,
+}
+
+fn parse_args() -> Result<Args, pico_args::Error> {
+    let mut args = pico_args::Arguments::from_env();
+    Ok(Args {
+        day: args.free_from_str()?,
+        year: args.opt_value_from_str(["-y", "--year"])?,
+    })
+}
+
+fn main() {
+    let args = match parse_args() {
+        Ok(args) => args,
+        Err(e) => {
+            eprintln!("Failed to process arguments: {}", e);
+            process::exit(1);
+        }
+    };
+
+    if aoc_cli::check().is_err() {
+        eprintln!("command \"aoc\" not found or not callable. Try running \"cargo install aoc-cli\" to install it.");
+        process::exit(1);
+    }
+
+    match aoc_cli::read(args.day, args.year) {
+        Ok(cmd_output) => {
+            if !cmd_output.status.success() {
+                process::exit(1);
+            }
+        }
+        Err(e) => {
+            eprintln!("failed to spawn aoc-cli: {}", e);
+            process::exit(1);
+        }
+    }
+}
---
+++ 2022/src/main.rs
@@ -0,0 +1,44 @@
+use advent_of_code::{ANSI_BOLD, ANSI_ITALIC, ANSI_RESET};
+use std::process::Command;
+
+fn main() {
+    let total: f64 = (1..=25)
+        .map(|day| {
+            let day = format!("{:02}", day);
+
+            let mut args = vec!["run", "--bin", &day];
+            if cfg!(not(debug_assertions)) {
+                args.push("--release");
+            }
+
+            let cmd = Command::new("cargo").args(&args).output().unwrap();
+
+            println!("----------");
+            println!("{}| Day {} |{}", ANSI_BOLD, day, ANSI_RESET);
+            println!("----------");
+
+            let output = String::from_utf8(cmd.stdout).unwrap();
+            let is_empty = output.is_empty();
+
+            println!(
+                "{}",
+                if is_empty {
+                    "Not solved."
+                } else {
+                    output.trim()
+                }
+            );
+
+            if is_empty {
+                0_f64
+            } else {
+                advent_of_code::parse_exec_time(&output)
+            }
+        })
+        .sum();
+
+    println!(
+        "{}Total:{} {}{:.2}ms{}",
+        ANSI_BOLD, ANSI_RESET, ANSI_ITALIC, total, ANSI_RESET
+    );
+}
