6b3a157a90393158b0ad896b9d267f19c7e7c8e7
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2024-12-03 23:20:43 -0800

    refactor: clean up 2024 day 4

--- 2024/day04/day04.py
+++ 2024/day04/day04.py
@@ -2,40 +2,33 @@ import sys
 import pathlib
 
 
-def check(grid, r, c, word):
-    n = len(word)
-    count = 0
+def search(grid, r, c, word):
+    count, n = 0, len(word)
 
     # horizontal
     if c + n <= len(grid[0]):
-        s = ''.join(grid[r][c:c+4])
-        count += s == word
+        count += ''.join(grid[r][c:c+4]) == word
 
     # vertical
     if r + n <= len(grid):
-        s = ''.join(grid[r+i][c] for i in range(n))
-        count += s == word
+        count += ''.join(grid[r+i][c] for i in range(n)) == word
 
     # diagonal down
     if (c + n <= len(grid[0])) and (r + n <= len(grid)):
-        s = ''.join(grid[r+i][c+i] for i in range(n))
-        count += s == word
+        count += ''.join(grid[r+i][c+i] for i in range(n)) == word
 
     # diagonal up
     if (c + n <= len(grid[0])) and (r - n + 1 >= 0):
-        s = ''.join(grid[r-i][c+i] for i in range(n))
-        count += s == word
+        count += ''.join(grid[r-i][c+i] for i in range(n)) == word
 
     return count
 
 
-def check2(grid, r, c):
+def search2(grid, r, c):
     if c + 1 >= len(grid[0]) or r + 1 >= len(grid) or r - 1 < 0 or c - 1 < 0:
         return 0
-
     s1 = ''.join(grid[r-1+i][c-1+i] for i in range(3))
     s2 = ''.join(grid[r+1-i][c-1+i] for i in range(3))
-
     return (s1 == "MAS" or s1 == "SAM") and (s2 == "MAS" or s2 == "SAM")
 
 
@@ -44,9 +37,9 @@ def part1(grid):
     for r, row in enumerate(grid):
         for c, let in enumerate(row):
             if let == 'X':
-                count += check(grid, r, c, "XMAS")
-            if let == 'S':
-                count += check(grid, r, c, "SAMX")
+                count += search(grid, r, c, "XMAS")
+            elif let == 'S':
+                count += search(grid, r, c, "SAMX")
     return count
 
 
@@ -56,7 +49,7 @@ def part2(grid):
     for r, row in enumerate(grid):
         for c, let in enumerate(row):
             if let == 'A':
-                count += check2(grid, r, c)
+                count += search2(grid, r, c)
     return count
 
 
