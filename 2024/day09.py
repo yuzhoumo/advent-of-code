15179534ca96035b68d717f2bf6684f77374ae88
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2024-12-09 00:16:33 -0800

    refactor: clean up 2024 day 9

--- 2024/day09.py
+++ 2024/day09.py
@@ -1,82 +1,55 @@
 import aoc
 
 
-def compact(disk):
-    res = []
-    i = 0
-    while i < len(disk):
-        while disk[-1] is None:
-            disk.pop()
-        if disk[i] is not None:
-            res.append(disk[i])
-        else:
-            res.append(disk.pop())
-        i += 1
-    return res
-
-
-def checksum(disk):
-    checksum = 0
-    for i, d in enumerate(disk):
-        if d != -1:
-            checksum += i * d
-    return checksum
-
-
 def parse_disk(data):
-    id = 0
-    disk = []
+    id, disk = 0, []
     for i in range(0, len(data) - 1, 2):
         size, free = int(data[i]), int(data[i+1])
-        disk.extend([id] * size)
-        disk.extend([None] * free)
-        id += 1
-    disk.extend([id] * int(data[-1]))
-    return disk
-
-
-def parse_disk2(data):
-    id = 0
-    disk = []
-    for i in range(0, len(data) - 1, 2):
-        size, free = int(data[i]), int(data[i+1])
-        disk.append((id, size))
-        disk.append((-1, free))
+        disk.extend([(id, size), (-1, free)])
         id += 1
     disk.append((id, int(data[-1])))
     return disk
 
 
+def get_mem(disk):
+    return [id for id, size in disk for _ in range(size)]
+
+
+def checksum(disk):
+    return sum(i * d for i, d in enumerate(disk) if d != -1)
+
+
 def part1(data):
-    res = compact(parse_disk(data))
+    mem = get_mem(parse_disk(data))
+    res, i, j = [], 0, len(mem) - 1
+    while i < j:
+        while mem[j] == -1: j -= 1
+        res.append(mem[i] if mem[i] != -1 else mem[j])
+        i += 1
     return checksum(res)
 
 
 def part2(data):
-    disk = parse_disk2(data)
+    def find_free_idx(size, end):
+        for i in range(end):
+            id_i, size_i = disk[i]
+            if id_i == -1 and size_i >= size:
+                return i
+
+    disk = parse_disk(data)
     for j in range(len(disk) - 1, -1, -1):
-        ij, sj = disk[j]
-        if ij == -1: continue
+        id_j, size_j = disk[j]
+        if id_j == -1: continue
 
-        for i in range(j):
-            ii, si = disk[i]
-            if ii != -1: continue
+        i = find_free_idx(size_j, j)
+        if i is None: continue
 
-            if si > sj:
-                disk[i] = (ii, si - sj)
-                disk[j] = (-1, sj)
-                disk.insert(i, (ij, sj))
-                break
-            elif si == sj:
-                disk[j] = (-1, sj)
-                disk[i] = (ij, sj)
-                break
+        id_i, size_i = disk[i]
+        disk[i] = (id_i, size_i - size_j)
+        disk[j] = (-1, size_j)
+        disk.insert(i, (id_j, size_j))
 
-    res = []
-    for d in disk:
-        res.extend([d[0]] * d[1])
-
-    return checksum(res)
+    return checksum(get_mem(disk))
 
 
 def main(text):
