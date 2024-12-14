9b57930a943e44880c5c7f8af18f1b2a4668dde6
Joe Mo <49494600+yuzhoumo@users.noreply.github.com>
2024-12-14 10:58:27 -0800

    refactor: clean up 2024 day 10

--- 2024/day10.py
+++ 2024/day10.py
@@ -1,67 +1,47 @@
 import aoc
-from collections import deque, defaultdict
+from collections import deque
+
+
+def bfs(grid, r, c, check_seen=True):
+    seen = set()
+    dq = deque([(r, c)])
+
+    count = 0
+    while dq:
+        r, c = dq.popleft()
+
+        if check_seen and (r, c) in seen:
+            continue
+        elif check_seen:
+            seen.add((r, c))
+
+        if grid[r][c] == 9:
+            count += 1
+            continue
+
+        for dr, dc in aoc.directions:
+            nr, nc  = r + dr, c + dc
+            if aoc.in_bounds(grid, nr, nc) and (grid[nr][nc] - grid[r][c]) == 1:
+                dq.append((nr, nc))
+
+    return count
 
 
 def part1(grid):
-    dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-    in_bounds = lambda r, c: 0 <= r < len(grid) and 0 <= c < len(grid[0])
-
-    def bfs(r, c):
-        seen = set()
-        dq = deque([(r, c)])
-
-        count = 0
-        while dq:
-            r, c = dq.popleft()
-
-            if (r, c) in seen:
-                continue
-
-            seen.add((r, c))
-
-            if grid[r][c] == 9:
-                count += 1
-                continue
-
-            for dr, dc in dirs:
-                nr, nc  = r + dr, c + dc
-                if in_bounds(nr, nc) and ((grid[nr][nc] - grid[r][c]) == 1):
-                    dq.append((nr, nc))
-
-        return count
-
     res = 0
     for r, row in enumerate(grid):
         for c, n in enumerate(row):
             if n == 0:
-                res += bfs(r, c)
-
+                res += bfs(grid, r, c)
     return res
 
 
-
 def part2(grid):
-    dirs = [(1, 0), (-1, 0), (0, 1), (0, -1)]
-    in_bounds = lambda r, c: 0 <= r < len(grid) and 0 <= c < len(grid[0])
-
-    def dfs(r, c):
-        if grid[r][c] == 9:
-            return 1
-
-        count = 0
-        for dr, dc in dirs:
-            nr, nc = dr + r, dc + c
-            if in_bounds(nr, nc) and (grid[nr][nc] - grid[r][c]) == 1:
-                count += dfs(nr, nc)
-
-        return count
-
     res = 0
     for r, row in enumerate(grid):
         for c, n in enumerate(row):
             if n == 0:
-                res += dfs(r, c)
-
+                res += bfs(grid, r, c, check_seen=False)
     return res
 
 
